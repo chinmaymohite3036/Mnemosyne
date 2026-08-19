da5a5fe8049c674bd9eb33d0e3316b210d0fb7b9
Chinmay Mohite <chinmaymohite0@gmail.com>
2026-08-19 07:25:53 +0530

    Chunking all video files with Metadata

--- stt.py
+++ stt.py
@@ -7,7 +7,6 @@ result = model.transcribe(audio = "audios/sample.mp3",
                           task = "translate",
                           word_timestamps = False)
 
-print(result["segments"])
 chunks = []
 for segment in result["segments"]:
     chunks.append({"start": segment["start"], "end": segment["end"], "text": segment["text"]})
