fa64ea4414d1ec7b3fb38eb6d3bd963e4fe2ca46
Chinmay Mohite <chinmaymohite0@gmail.com>
2026-08-24 21:02:44 +0530

    Created dataframe of all embeddings of all videos

--- read_chunks.py
+++ read_chunks.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import json
+import pandas as pd
+
 
 def create_embedding(text_list):
     r = requests.post(
@@ -11,10 +13,13 @@ def create_embedding(text_list):
         }
     )
 
-    print("STATUS:", r.status_code)
+    if r.status_code != 200:
+        print("Ollama Error:", r.text)
+        return None
 
     embedding = r.json()["embeddings"]
     return embedding
+
 
 # list all jsons
 
@@ -25,23 +30,46 @@ chunk_id = 0
 for json_file in jsons:
     with open(f"jsons/{json_file}") as f:
         content = json.load(f)
+    print(f"Processing {json_file} with {len(content['chunks'])} chunks")
 
-    # gathering all the text from the chunks to create embeddings in one go
-    embeddings = create_embedding([c['text'] for c in content['chunks']])
+    # gathering all the text from the chunks
+    texts = [c['text'] for c in content['chunks']]
+
+    # create embeddings in batches
+    all_embeddings = []
+    batch_size = 150
+
+    for i in range(0, len(texts), batch_size):
+
+        batch = texts[i:i + batch_size]
+
+        print(f"Embedding chunks {i} to {i + len(batch) - 1}")
+
+        embeddings = create_embedding(batch)
+
+        if embeddings is None:
+            print("Embedding failed. Stopping program.")
+            break
+
+        all_embeddings.extend(embeddings)
 
     print("Number of chunks:", len(content["chunks"]))
-    print("Number of embeddings:", len(embeddings))
+    print("Number of embeddings:", len(all_embeddings))
 
     for i, chunk in enumerate(content['chunks']):
         chunk['chunk_id'] = chunk_id
-        chunk['embedding'] = embeddings[i]
+        chunk['embedding'] = all_embeddings[i]
+
         chunk_id += 1
         my_dicts.append(chunk)
-        print(chunk)
-    break
-
-# print(my_dicts)
 
 
-# a = create_embedding(["Chimo is Intelligent and Helpfull person!", "Chimo is a good person!"])
+print(my_dicts)
+
+df = pd.DataFrame.from_records(my_dicts)
+print(df)
+
+
+# a = create_embedding(["Chimo is Intelligent and Helpfull person!",
+#                       "Chimo is a good person!"])
 # print(a)
